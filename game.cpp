a2b511cf87e45271db560ad2bb69d9766762e0bb
Vlad <vlad.vedernikov03@mail.ru>
2024-10-17 15:22:22 +0300

    Открытие окна и заливка его цветом. Всё работает.

--- game.cpp
+++ game.cpp
@@ -1,56 +1,42 @@
-/*This source code copyrighted by Lazy Foo' Productions 2004-2024
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDHT = 640;
+const int SCREEN_HIGHT = 480;
 
-int main( int argc, char* args[] )
+int main(int argc, char *argv[])
 {
-	//The window we'll be rendering to
-	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
-
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+    SDL_Window* window = NULL;
+    SDL_Surface* screenSurface = NULL;
+    
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+        printf("SDL no Init!, SDL_ERROR: %s\n", SDL_GetError());
+    else 
+    {
+        window = SDL_CreateWindow("First Bloode", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDHT, SCREEN_HIGHT, SDL_WINDOW_SHOWN);
+        if(window == NULL)
+            printf("SDL no CREATE WINDOW! SDL_ERROR: %s", SDL_GetError());
+        else
+        {
+            screenSurface = SDL_GetWindowSurface(window);
+            SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 234, 111, 45));
+            SDL_UpdateWindowSurface(window);
             
-            //Hack to get window to stay up
-            SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
-		}
-	}
+            SDL_Event e;
+            bool quit = false;
 
-	//Destroy window
-	SDL_DestroyWindow( window );
-
-	//Quit SDL subsystems
-	SDL_Quit();
-
-	return 0;
+            while(quit == false)
+            {
+                while(SDL_PollEvent(&e))
+                {
+                    if(e.type == SDL_QUIT)
+                        quit = true;
+                }
+            }
+        }
+    }
+    SDL_DestroyWindow(window);
+    SDL_Quit(); 
+    
+    return 0;
 }
